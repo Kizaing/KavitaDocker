b98b66d74fcc3c80ff36c162f3b41e84f1410879
Chris Plaatjes <cplaatjes1581@conestogac.on.ca>
2021-04-28 01:05:24 -0400

    Removed build.sh fix, not needed

--- build_target.sh
+++ build_target.sh
@@ -8,10 +8,6 @@ git clone https://github.com/Kareadita/Kavita.git
 git clone https://github.com/Kareadita/Kavita-webui.git
 
 cd Kavita
-
-sed -i 's+../kavita-webui/+../Kavita-webui/+g' build.sh
-sed -i 's+../kavita/+../Kavita/+g' build.sh
-
 chmod +x build.sh
 
 #Builds program based on the target platform
@@ -20,10 +16,10 @@ if [ "$TARGETPLATFORM" == "linux/amd64" ]
 then
 	./build.sh linux-x64
 	mv /Projects/Kavita/_output/linux-x64 /Projects/Kavita/_output/build
-#elif [ "$TARGETPLATFORM" == "linux/arm/v7" ]
-#then
-#	./build.sh linux-arm
-#	mv /Projects/Kavita/_output/linux-arm /Projects/Kavita/_output/build
+elif [ "$TARGETPLATFORM" == "linux/arm/v7" ]
+then
+	./build.sh linux-arm
+	mv /Projects/Kavita/_output/linux-arm /Projects/Kavita/_output/build
 elif [ "$TARGETPLATFORM" == "linux/arm64" ]
 then
 	./build.sh linux-arm64
